36c0d284d422dc16b4e073aba8227da4a8aea562
jgonzalez-2023195 <jgonzalez-2023195@kinal.edu.gt>
2024-07-08 22:19:36 -0600

    Se crea la entidad de Clientes con sus atributos por José González

--- sql/DBBancoOficialKinal.sql
+++ sql/DBBancoOficialKinal.sql
@@ -1,5 +1,4 @@
 Drop database if exists DBBancoOficialKinal;
-Drop database if exists DBBancoOficialKinal;
 create database DBBancoOficialKinal;
 
 use DBBancoOficialKinal;
@@ -11,4 +10,17 @@ create table TipoCuenta(
     tazaDeInteres double(10,2) not null,
     tazaDeImpuestos double(10,2) not null,
     primary key PK_codigoTipoCuenta(codigoTipoCuenta)
+);
+
+create table Clientes(
+	codigoCliente int not null auto_increment,
+    nombreCliente varchar(20) not null,
+    apellidoCliente varchar(20) not null,
+    direccionCliente varchar(60) not null,
+    telefonoCliente varchar(8) not null,
+    correoCliente varchar(20) not null,
+    descripcion varchar(100) not null,
+    codigoTipoCuenta int not null,
+    primary key PK_codigoCliente(codigoCliente),
+    constraint FK_Clientes_TipoCuenta foreign key(codigoTipoCuenta) references TipoCuenta(codigoTipoCuenta)
 );
